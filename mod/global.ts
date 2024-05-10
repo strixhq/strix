b327b9dc752b48619c2ca68ab47cbf26f05a6d12
ihasq <gsd79d2mtizifb7cv8d119a9xpq3ja@gmail.com>
2024-05-10 13:05:58 +0900

    wip

--- mod/global.ts
+++ mod/global.ts
@@ -1,18 +1,23 @@
+const propMap = {};
+
 export const globalGetter = new Proxy(Object.create(null), {
 
 	get: (t, prop: string) =>
- 
-		window[prop] ||
 
-		(() => {
+		propMap[prop] ||
+ 
+		(propMap[prop] = window[prop]) ||
+
+		(propMap[prop] = (() => {
 			const splittedPropBuffer = prop.split("_");
 			const splittedPropBufferLength = splittedPropBuffer.length;
 		
-			let currentObjectCursor = window;
+			let currentObjectCursor = globalThis;
+
 			for(let propIndex = 0; propIndex < splittedPropBufferLength; propIndex++) {
 				currentObjectCursor = currentObjectCursor[splittedPropBuffer[propIndex]];
 			}
 
 			return currentObjectCursor;
-		})()
-})
+		})())
+});
