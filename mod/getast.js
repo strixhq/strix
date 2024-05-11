fd522d4aec9b00cdac2f2e6b5722a217915b9522
ihasq <gsd79d2mtizifb7cv8d119a9xpq3ja@gmail.com>
2024-05-11 13:59:15 +0900

    wip

--- mod/getast.js
+++ mod/getast.js
@@ -1,46 +1,59 @@
+import { parseAST } from "./parseast";
+
 /**
  * 
  * @param { TemplateStringsArray } TemplateStringsArray 
- * @param { WeakMap } TSAStructureMap
+ * @param { WeakMap } TSAElementMap
  * @param { object } TSAFragmentMap
+ * @param { WeakMap } TSAStructureMap
  */
 
 export const getAST = (
 
 	TemplateStringsArray,
-	TSAStructureMap,
 
+	TSAElementMap,
 	TSAFragmentMap,
+	TSAStructureMap
 
 ) => {
 	// WeakMapから取得
-	return TSAStructureMap.get(TemplateStringsArray) || (() => {
+	return TSAElementMap.get(TemplateStringsArray) || (() => {
 
 		// オブジェクトがWeakMapのキーに見つからなかった場合、TemplateStringsArrayをインデックスから探索する
 
 		// TemplateStringsArrayのlengthプロパティをキャッシュする
 		const TSALength = TemplateStringsArray.length;
-		let TSAFragmentBuffer = TSAFragmentMap;
+		let TSAFragmentMapBuffer = TSAFragmentMap;
 
 		for(let i = 0; i < TSALength; i++) {
 
-			const TSAFragment = TemplateStringsArray[i];
-			const TSAFragmentRef = TSAFragmentBuffer[TSAFragment]
+			const TSAFragmentRef = TSAFragmentMapBuffer[TSAFragment]
 
 			if(!TSAFragmentRef) {
-
 				// 未パースのパターンであることが確定したので、探索用のforループから構築用のforループに移行し、参照オブジェクトを新規作成する
+
+				const TSAStructure = parseAST(TemplateStringsArray);
+
 				for(let j = i; j < TSALength; j++) {
-					TSAFragmentBuffer[TSAFragment][TemplateStringsArray[j + 1]] = {};
+					const TSAFragment = TemplateStringsArray[j];
+					TSAFragmentMapBuffer[TSAFragment] = {};
+					TSAFragmentMapBuffer = TSAFragmentMapBuffer[TSAFragment];
 				}
-	
-				break;
+
+				TSAStructureMap.set(TSAFragmentMapBuffer, TSAStructure);
+				TSAElementMap.set(TemplateStringsArray, TSAStructure);
+
+				return TSAStructure;	
 			}
 
-			TSAFragmentBuffer = TSAFragmentRef;
+			TSAFragmentMapBuffer = TSAFragmentRef;
 
 		}
 
-		return TSAStructureMap.get(TSAFragmentBuffer);
+		const TSAStructure = TSAStructureMap.get(TSAFragmentMapBuffer);
+		TSAElementMap.set(TemplateStringsArray, TSAStructure);
+
+		return TSAStructure;
 	})();
 }
