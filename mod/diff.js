120125ddb2e953a8c3b96bb4d54b4ade368d06fd
ihasq <gsd79d2mtizifb7cv8d119a9xpq3ja@gmail.com>
2024-05-16 17:45:02 +0900

    wip

---
+++ mod/diff.js
@@ -0,0 +1,28 @@
+import { WeakMap } from "./keyword/global"
+
+const DIFF_BUFFER = new WeakMap();
+
+const createDiff = (
+
+	ELEMENT_TSA_FROM,
+	ELEMENT_TSA_TO,
+
+) => {
+
+}
+
+/**
+ * 
+ * @param { TemplateStringsArray } ELEMENT_TSA_FROM 
+ * @param { TemplateStringsArray } ELEMENT_TSA_TO 
+ */
+
+export const getDiff = (
+
+	ELEMENT_TSA_FROM,
+	ELEMENT_TSA_TO,
+
+) => {
+
+
+}
