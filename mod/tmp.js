33bb06402ab5b72d5b405d4ec71142456024c5e5
ihasq <gsd79d2mtizifb7cv8d119a9xpq3ja@gmail.com>
2024-05-21 17:18:48 +0900

    wip

--- mod/tmp.js
+++ mod/tmp.js
@@ -4,12 +4,4 @@
  * @returns { (a: TemplateStringsArray, ...b: any[]) => (j: number) => (TemplateStringsArray | any[]) }
  */
 
-export const tmp = i => (
-
-	a,
-	...b
-
-) => j => i ^ j
-
-	? a
-	: b
+export const tmp = i => (a, ...b) => j => i ^ j ? a : b
