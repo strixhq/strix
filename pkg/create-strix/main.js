49bdc11af6343f8a4052cea7be3908b7f8e313c8
ihasq <gsd79d2mtizifb7cv8d119a9xpq3ja@gmail.com>
2024-08-23 09:56:57 +0900

    wip

--- pkg/create-strix/main.js
+++ pkg/create-strix/main.js
@@ -2,54 +2,51 @@ import * as prompts from '@clack/prompts';
 import * as fs from "node:fs";
 import * as process from "node:process"
 
-console.log("\n🦉 \x1b[01mWelcome to Strix!\x1b[0m 🦉")
+console.log("\n🦉 \x1b[01mWelcome to Strix!\x1b[0m 🦉");
 
-const name = await prompts.text({
-	message: '🦉💬 PROJECT NAME:',
-	placeholder: 'my-strix-project', // デフォルト値
-	validate(value) {
-		// バリデーション
-		if(fs.existsSync(`./${value}`)) {
-			return `The directory named ${value} already exists`
+(async () => {
+	const name = await prompts.text({
+		message: '🦉💬 PROJECT NAME:',
+		placeholder: 'my-strix-project', // デフォルト値
+		validate(value) {
+			// バリデーション
+			if(fs.existsSync(`./${value}`)) {
+				return `The directory named ${value} already exists`
+			}
+		},
+	});
+	
+	// const language = await prompts.select({
+	// 	message: '🦉💬 LANGUAGE:',
+	// 	options: [
+	// 		{ value: 'js', label: "\x1b[01;93mJavaScript\x1b[0m" },
+	// 		{ value: 'ts', label: "\x1b[01;94mTypeScript\x1b[0m" },
+	// 	],
+	// });
+	
+	// const template = await prompts.select({
+	// 	message: '🦉💬 TEMPLATE USAGE:',
+	// 	options: [
+	// 		{ value: true, label: "\x1b[01;93mPre-built template\x1b[0m" },
+	// 		{ value: false, label: "\x1b[01;94mBlank project\x1b[0m" },
+	// 	],
+	// });
+	
+	const __dirname = process.cwd();
+	
+	fs.mkdirSync(`${__dirname}/${name}`);
+	fs.cpSync(`./temp/js`, `${__dirname}/${name}`, { recursive: true });
+	fs.writeFileSync(`${__dirname}/${name}/package.json`, `{
+		"name": "${name}",
+		"version": "0.0.1",
+		"scripts": {
+			"start": "npx jsr add @strix/std @strix/client && npx vite"
 		}
-	},
-});
-
-// const language = await prompts.select({
-// 	message: '🦉💬 LANGUAGE:',
-// 	options: [
-// 		{ value: 'js', label: "\x1b[01;93mJavaScript\x1b[0m" },
-// 		{ value: 'ts', label: "\x1b[01;94mTypeScript\x1b[0m" },
-// 	],
-// });
-
-// const template = await prompts.select({
-// 	message: '🦉💬 TEMPLATE USAGE:',
-// 	options: [
-// 		{ value: true, label: "\x1b[01;93mPre-built template\x1b[0m" },
-// 		{ value: false, label: "\x1b[01;94mBlank project\x1b[0m" },
-// 	],
-// });
-
-const __dirname = process.cwd();
-
-fs.mkdirSync(`${__dirname}/${name}`);
-fs.cpSync(`./temp/js`, `${__dirname}/${name}`, { recursive: true });
-fs.writeFileSync(`${__dirname}/${name}/package.json`, `{
-	"name": "${name}",
-	"version": "0.0.1",
-	"imports": {
-		"@strix/std": "./node_modules/lib_strix/std/mod.js",
-		"@strix/client": "./node_modules/lib_strix/client/mod.js"
-	},
-	"scripts": {
-		"start": "npx jsr add @strix/std @strix/client"
-	}
-}`)
-
-console.log(`\n🦉 \x1b[01mDone. now, enter these following commands: \x1b[0m 🦉
-╭─────────────────╮
-│ $ ${`cd ${name}`.padEnd(14, "\u0020")}│
-│ $ npm start     │
-╰─────────────────╯
-`)
+	}`)
+	
+	console.log(`\n🦉 \x1b[01mDone. now, enter these following commands: \x1b[0m 🦉
+  ╭${"─".repeat(name.length + 13)}╮
+  │ > cd ${name + "\u0020".repeat(7)}│
+  │ > npm start${"\u0020".repeat(1 + name.length)}│
+  ╰${"─".repeat(name.length + 13)}╯`)
+})()
