bfb84f102078d4bd974ab3df97aaf0f506b7d2a4
ihasq <gsd79d2mtizifb7cv8d119a9xpq3ja@gmail.com>
2024-08-25 00:19:34 +0900

    wip

--- pkg/std/mod.js
+++ pkg/std/mod.js
@@ -2,4 +2,4 @@ export { h } from "./src/h.js";
 export { on } from "./src/on.js";
 export { at } from "./src/at.js";
 
-export { $ } from "@ihasq/esptr";
+export { $ } from "jsr:@ihasq/esptr@latest";
