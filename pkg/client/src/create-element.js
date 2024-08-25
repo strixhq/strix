81a751fccd2e0f7dfd30dc754b191c0dca278591
ihasq <gsd79d2mtizifb7cv8d119a9xpq3ja@gmail.com>
2024-08-25 16:48:33 +0900

    wip

--- pkg/client/src/create-element.js
+++ pkg/client/src/create-element.js
@@ -1,14 +1,102 @@
 import { getRandomToken } from "@strix/core"
 
+const resolveTemplate = ([{ raw: TSA }, TVA], TSA_UUID, TSA_REGEX, TSA_BEGIN_INDEX) => {
+
+	const
+		TVA_LENGTH = TVA.length,
+		ATTR_INDEX = [],
+		TVA_CALLBACKS = []
+	;
+
+	let BUF_STR;
+		
+	(BUF_STR = TSA
+		.map((x, i) => x + (i == TVA_LENGTH ? "" : `id="${TSA_UUID}-${
+			(TSA_BEGIN_INDEX + i)
+				.toString(36)
+				.padStart(8, "0")
+		}"`))
+		.join("")
+	)
+		.matchAll(TSA_REGEX)
+		.forEach(([matchedTemp]) => {
+			ATTR_INDEX.push(parseInt(matchedTemp.slice(-10, -2), 36))
+		})
+	;
+
+	return {
+		formattedTemplate: BUF_STR,
+		initializerCallbacks: Array(TVA_LENGTH).fill(0).map((_, i) => 
+			ATTR_INDEX.includes(TSA_BEGIN_INDEX + i)
+				? () => {
+
+				}
+				: () => {
+					
+				}
+		)
+	}
+
+	// for(let TVA_index = 0; TVA_index < TVA_LENGTH; TVA_index++) {
+
+	// 	const
+	// 		ID_STR = `${TSA_UUID}-${TVA_index.toString(36).padStart(8, "0")}`
+	// 		TOKEN_STR = `id="${ID_STR}"`,
+	// 		TVA_BUF = TVA[TVA_index],
+	// 		TVA_BUF_TYPE = (
+	// 			typeof TVA_BUF == "object" &&
+	// 			typeof TVA_BUF.toString() == "symbol" &&
+	// 			TVA_BUF in window
+	// 		)
+	// 			? "POINTER"
+	// 			: "PRIMITIVE"
+	// 	;
+
+	// 	if(ATTR_INDEX.includes(TVA_index)) {
+	// 		BUF_STR = BUF_STR.replace(TOKEN_STR, `<span ${TOKEN_STR}></span>`);
+	// 		TVA_CALLBACKS.push(() => {
+	// 			const TEXT_BUF = new Text();
+	// 			BASE_TEMP.querySelector(`#${ID_STR}`).replaceWith(TEXT_BUF);
+	// 			if(TVA_BUF_TYPE == "POINTER") {
+	// 				TEXT_BUF.textContent = window[TVA_BUF];
+	// 				TVA_BUF.watch(newValue => TEXT_BUF.textContent = newValue);
+	// 			} else {
+	// 				TEXT_BUF.textContent = TVA_BUF;
+	// 			}
+	// 		})
+	// 	} else {
+	// 		TVA_CALLBACKS.push(() => {
+	// 			console.log(TVA_BUF)
+	// 			const TEMP_BUF = BASE_TEMP.querySelector(`#${ID_STR}`);
+	// 			Reflect.ownKeys(TVA_BUF).forEach(x => {
+	// 				if(typeof x == "symbol" && x in window) {
+	// 					window[x](TVA_BUF[x], TEMP_BUF);
+	// 				} else {
+	// 					TEMP_BUF[x] = TVA_BUF[x];
+	// 				}
+	// 			})
+
+	// 		})
+	// 	}
+	// }
+
+	// return {
+	// 	tempString: BUF_STR
+	// }
+}
+
 const BASE_DF = document.createDocumentFragment();
 
 const createElement = ([{ raw: TSA }, TVA]) => {
 
 	const
 		TSA_UUID = "strix-" + getRandomToken(32),
-		TSA_LENGTH = TSA.length - 1,
+		// TSA_LENGTH = TSA.length - 1,
+		TSA_REGEX = new RegExp(`<[a-z\-]+ id="${TSA_UUID}-[a-z0-9]+">`, "g"),
 		TVA_LENGTH = TVA.length,
-		BASE_TEMP = document.createElement("div")
+		BASE_TEMP = document.createElement("div"),
+		BUF_RESULT = [],
+		ATTR_INDEX = []
 	;
 
 	BASE_DF.appendChild(BASE_TEMP);
@@ -30,25 +118,18 @@ const createElement = ([{ raw: TSA }, TVA]) => {
 					: BUF_TVA
 				;
 
-				return x + (i == TSA_LENGTH ? "" : `id="${TSA_UUID}-${i.toString(36).padStart(8, "0")}"`)
+				return x + (i == TVA_LENGTH ? "" : `id="${TSA_UUID}-${i.toString(36).padStart(8, "0")}"`)
 			})
 			.join("")
-		,
-		SEL_BUF,
-		COMMENT_BUF
 	;
 
-	const BUF_RESULT = [];
-
-	const TSA_REGEX = new RegExp(`<[a-z\-]+ id="${TSA_UUID}-[a-z0-9]+">`, "g");
-
-	const ATTR_INDEX = [];
+	console.log(BUF_STR);
 
 	BUF_STR.matchAll(TSA_REGEX).forEach(({ 0: matchedTemp, index }, i) => {
 		ATTR_INDEX.push(parseInt(matchedTemp.slice(-10, -2), 36))
 	});
 
-	const PLACEHOLDER_UUID = getRandomToken(32);
+	console.log(ATTR_INDEX)
 
 	for(let TVA_index = 0; TVA_index < TVA_LENGTH; TVA_index++) {
 
@@ -65,15 +146,29 @@ const createElement = ([{ raw: TSA }, TVA]) => {
 				: "PRIMITIVE"
 		;
 
-		if(!ATTR_INDEX.includes(TVA_index)) {
-			BUF_STR = BUF_STR.replace(TOKEN_STR, `<span ${TOKEN_STR} ${PLACEHOLDER_UUID}></span>`);
+		if(ATTR_INDEX.includes(TVA_index)) {
+			BUF_STR = BUF_STR.replace(TOKEN_STR, `<span ${TOKEN_STR}></span>`);
 			BUF_RESULT.push(() => {
 				const TEXT_BUF = new Text();
-				const TEMP_BUF = BASE_TEMP.querySelector(`#${ID_STR}`);
+				BASE_TEMP.querySelector(`#${ID_STR}`).replaceWith(TEXT_BUF);
+				if(TVA_BUF_TYPE == "POINTER") {
+					TEXT_BUF.textContent = window[TVA_BUF];
+					TVA_BUF.watch(newValue => TEXT_BUF.textContent = newValue);
+				} else {
+					TEXT_BUF.textContent = TVA_BUF;
+				}
 			})
 		} else {
 			BUF_RESULT.push(() => {
+				console.log(TVA_BUF)
 				const TEMP_BUF = BASE_TEMP.querySelector(`#${ID_STR}`);
+				Reflect.ownKeys(TVA_BUF).forEach(x => {
+					if(typeof x == "symbol" && x in window) {
+						window[x](TVA_BUF[x], TEMP_BUF);
+					} else {
+						TEMP_BUF[x] = TVA_BUF[x];
+					}
+				})
 
 			})
 		}
@@ -81,7 +176,11 @@ const createElement = ([{ raw: TSA }, TVA]) => {
 
 	BASE_TEMP.innerHTML = BUF_STR;
 
-	// BUF_RESULT.forEach(x => x());
+	BUF_RESULT.forEach(x => x());
+
+	// queueMicrotask(BASE_TEMP.remove);
+
+	return BASE_TEMP.childNodes;
 
 	// for(let TVA_index = 0; TVA_index < TVA_LENGTH; TVA_index++) {
 	// 	const TOKEN_STR = `id="${TSA_UUID}-${TVA_index}"`;
