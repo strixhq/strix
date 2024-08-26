2803eb3043d50539d6863d291201055154018ce6
ihasq <gsd79d2mtizifb7cv8d119a9xpq3ja@gmail.com>
2024-08-26 23:35:18 +0900

    wip

--- pkg/std/src/on.js
+++ pkg/std/src/on.js
@@ -1,12 +1,10 @@
 import { $ } from "jsr:@ihasq/esptr@0.1.0"
 
 export const on = new Proxy(
-	$((value, ref) => {
-		Object.keys(value).forEach(eventName => ref.addEventListener(eventName, value[eventName], { passive: true }))
-	}),
+	{},
 	{
-		get: (t, prop) => $((value, ref) => {
-			ref.addEventListener(prop, value, { passive: true })
-		})
+		get: (t, prop) => prop == "toString"
+			? () => $((value, ref) => Object.keys(value).forEach(x => ref.addEventListener(x, value[x], { passive: true })))
+			: $((value, ref) => ref.addEventListener(prop, value, { passive: true }))
 	}
 )
