eef84ab2100b01a8b2b9cbda289d774b9075bd75
ihasq <gsd79d2mtizifb7cv8d119a9xpq3ja@gmail.com>
2024-09-01 23:44:38 +0900

    wip

---
+++ task/build.js
@@ -0,0 +1,26 @@
+import * as esbuild from "npm:esbuild@0.20.2";
+// Import the WASM build on platforms where running subprocesses is not
+// permitted, such as Deno Deploy, or when running without `--allow-run`.
+// import * as esbuild from "https://deno.land/x/esbuild@0.20.2/wasm.js";
+
+import { denoPlugins } from "jsr:@luca/esbuild-deno-loader@^0.10.3";
+
+await esbuild.build({
+  plugins: [...denoPlugins()],
+  entryPoints: ["./pkg/std/mod.js"],
+  outfile: "./dist/std.mod.js",
+  bundle: true,
+  minify: true,
+  format: "esm",
+});
+
+await esbuild.build({
+  plugins: [...denoPlugins()],
+  entryPoints: ["./pkg/client/mod.js"],
+  outfile: "./dist/client.mod.js",
+  bundle: true,
+  minify: true,
+  format: "esm",
+});
+
+esbuild.stop();
