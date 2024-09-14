892c3d33fe13a82689e663e84c10134585558ab8
ihasq <gsd79d2mtizifb7cv8d119a9xpq3ja@gmail.com>
2024-09-14 15:13:22 +0900

    wip

--- task/build.ts
+++ task/build.ts
@@ -5,12 +5,14 @@ import * as esbuild from 'npm:esbuild@0.20.2';
 
 import { denoPlugins } from 'jsr:@luca/esbuild-deno-loader@^0.10.3';
 
+const minify = true;
+
 await esbuild.build({
 	plugins: [...denoPlugins()],
 	entryPoints: ['./pkg/std/mod.ts'],
 	outfile: './dist/std.mod.js',
 	bundle: true,
-	// minify: true,
+	minify,
 	treeShaking: true,
 	format: 'esm',
 });
@@ -20,7 +22,7 @@ await esbuild.build({
 	entryPoints: ['./pkg/client/mod.ts'],
 	outfile: './dist/client.mod.js',
 	bundle: true,
-	// minify: true,
+	minify,
 	treeShaking: true,
 	format: 'esm',
 });
@@ -30,7 +32,7 @@ await esbuild.build({
 	entryPoints: ['./pkg/attr/mod.ts'],
 	outfile: './dist/attr.mod.js',
 	bundle: true,
-	// minify: true,
+	minify,
 	treeShaking: true,
 	format: 'esm',
 });
