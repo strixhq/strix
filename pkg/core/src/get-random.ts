0c558562e6dbbf5708934452ea7dba22a8638e2e
ihasq <gsd79d2mtizifb7cv8d119a9xpq3ja@gmail.com>
2024-09-24 11:52:19 +0900

    wip

--- pkg/core/src/get-random.ts
+++ pkg/core/src/get-random.ts
@@ -1,37 +1,22 @@
 const BUF_SIZE = 1024;
-const RAND_BUF = crypto.getRandomValues(new Uint32Array(BUF_SIZE / 4));
+const RAND_BUF = crypto.getRandomValues(new Uint8Array(BUF_SIZE));
 
 let index = 0;
+
+const refreshRandBuf = (): number => (crypto.getRandomValues(RAND_BUF), index = 0)
 
 const getBufferFragment = (): number => RAND_BUF[
 	
 	index == BUF_SIZE
 
-		? ((): number => {
-			crypto.getRandomValues(RAND_BUF);
-			return index = 0;
-		})()
+		? refreshRandBuf()
 
 		: index++
 
 ];
 
-export const random = (
+export const getRandom = (
 
 	length: number = 16,
-	type: string = "string",
-	sample: string = "0123457689abcdefghijklmnopqrstuvwxyz",
-	sampleLength: number = sample.length,
 
-): string => {
-	const result = Array(length)
-		.fill(0)
-		.map(getBufferFragment)
-	;
-
-	return type == "string"
-		? result.map(x => sample[x / (2 ^ 32) * sampleLength]).join("")
-		: type == "charcode"
-		? String.fromCharCode(...result)
-		: ""
-}
+): string => String.fromCharCode.apply(null, Array(length).fill(0).map(getBufferFragment))
