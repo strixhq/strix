4e963918bf0328052f7840f9e1a02cf9c6650378
ihasq <gsd79d2mtizifb7cv8d119a9xpq3ja@gmail.com>
2024-09-26 14:24:30 +0900

    wip

--- pkg/client/mod.ts
+++ pkg/client/mod.ts
@@ -2,4 +2,4 @@ type StrixHTMLFragment = [TemplateStringsArray, any[], symbol]
 
 export * from './src/create-element.ts'
 // export { writeBody } from './src/write-body.ts';
-export * from './src/write.ts'
+// export * from './src/write.ts'
