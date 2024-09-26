ad825b569c0c26412da64033257825615f1223bb
ihasq <gsd79d2mtizifb7cv8d119a9xpq3ja@gmail.com>
2024-09-26 15:28:24 +0900

    wip

--- pkg/core/src/get-random.ts
+++ pkg/core/src/get-random.ts
@@ -1,36 +1,29 @@
-const BUF_SIZE = 1024;
-const RAND_BUF = crypto.getRandomValues(new Uint8Array(BUF_SIZE));
-
 let index = 0;
 
-const refreshRandBuf = (): number => (crypto.getRandomValues(RAND_BUF), index = 0)
+const BUF_SIZE = 1024,
+	RAND_BUF = crypto.getRandomValues(new Uint8Array(BUF_SIZE)),
+	refreshRandBuf = (): number => (crypto.getRandomValues(RAND_BUF), index = 0),
+	getBufferFragment = (max: number = 1): number => Math.floor(RAND_BUF[
+		index == BUF_SIZE ? refreshRandBuf() : index++
+	] / 255 * max),
+	TEMP_CTRL: number[] = Array.from({ length: 32 }, (_, index): number => (index += 127) + Number(index >= 142) + Number(index >= 157)),
+	TEMP_STR: number[] = "0123456789abcdefghijklmnopqrstuvwxyz".split("").map(x => x.charCodeAt(0)),
+	getRandom = (
 
-const TEMP_BAN: number[] = [142, 158];
-const TEMP: string = String.fromCharCode.apply(
-	null,
-	Array
+		length: number = 16,
+		type: string = "ctrl"
+
+	): string => String.fromCharCode.apply(null, Array
 		.from(
-			{ length: 33 },
-			(_, i): (number | boolean) => TEMP_BAN.includes(i += 127) ? false : i
+			{ length },
+			(): number =>
+					type == "ctrl" 
+					? TEMP_CTRL[Math.floor(getBufferFragment(32))]
+					: type == "str"
+					? TEMP_STR[Math.floor(getBufferFragment(36))]
+					: 0
 		)
-		.filter(Boolean) as number[]
-)
+	)
+;
 
-const getBufferFragment = (): number => RAND_BUF[
-	
-	index == BUF_SIZE
-
-		? refreshRandBuf()
-
-		: index++
-
-];
-
-export const getRandom = (
-
-	length: number = 16,
-
-): string => Array(length)
-	.fill(0)
-	.map(() => TEMP[Math.floor((getBufferFragment() / 255) * 31)])
-	.join("")
+export { getRandom }
