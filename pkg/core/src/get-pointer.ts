05418f8b7924b199fbeddc077ecae6324c2e1e7f
ihasq <gsd79d2mtizifb7cv8d119a9xpq3ja@gmail.com>
2024-09-27 14:45:37 +0900

    wip

--- pkg/core/src/get-pointer.ts
+++ pkg/core/src/get-pointer.ts
@@ -1,3 +1,3 @@
 const symbolBuf: any = {};
 
-export const getPointer = (symbol: symbol): (object | undefined) => symbolBuf[symbol] ||= globalThis[symbol.description?.slice(0, 16)]?.(symbol)
+export const getPointer = (symbol: symbol): (object | undefined) => symbolBuf[symbol] ||= globalThis[symbol.description.slice(0, 16)]?.(symbol)
