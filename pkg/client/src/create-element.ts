9597d17a40bf10290fe212b488ead10de08e2b7c
ihasq <gsd79d2mtizifb7cv8d119a9xpq3ja@gmail.com>
2024-10-27 11:41:48 +0900

    wip

--- pkg/client/src/create-element.ts
+++ pkg/client/src/create-element.ts
@@ -3,7 +3,7 @@ import { createNode } from './create-node.ts'
 
 const { STRIX_HTML_IDENTIFIER } = getEnv
 
-export const createElement = (fragment: TemplateStringsArray | Function): NodeList =>
+export const createElement = (fragment: object | Function): NodeList =>
 	createNode(
 		typeof fragment == 'function' ? fragment() : fragment[2] == STRIX_HTML_IDENTIFIER ? fragment : undefined,
 		document.createElement('strix-unregistered'),
